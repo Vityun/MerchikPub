1f22a5686089a056f9bc6f2c65b68b8a72a6094f
Viktor <thevityun@gmail.com>
2022-12-02 16:28:56 +0200

    1. Download WpDAta and Options from 14 days

--- app/src/main/java/ua/com/merchik/merchik/ServerExchange/TablesLoadingUnloading.java
+++ app/src/main/java/ua/com/merchik/merchik/ServerExchange/TablesLoadingUnloading.java
@@ -348,7 +348,8 @@ public class TablesLoadingUnloading {
 
         String mod = "plan";
         String act = "list";
-        String date_from = timeYesterday7;
+        String date_from = Clock.getDatePeriod(-14);
+//        String date_from = timeYesterday7;
         String date_to = timeTomorrow;
 
         ProgressDialog pg = ProgressDialog.show(context, "Обмен данными с сервером.", "Обновление таблицы: " + "План работ", true, true);
@@ -598,7 +599,8 @@ public class TablesLoadingUnloading {
 
         String mod = "plan";
         String act = "options_list";
-        String date_from = timeYesterday7;
+//        String date_from = timeYesterday7;
+        String date_from = Clock.getDatePeriod(-14);
         String date_to = timeTomorrow;
 
         ProgressDialog pg = ProgressDialog.show(context, "Обмен данными с сервером.", "Обновление таблицы: " + "Опции", true, true);
