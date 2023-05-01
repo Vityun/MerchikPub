8b231397c1851fab1810ff105b92329e2031dd80
Viktor <thevityun@gmail.com>
2023-05-01 13:30:03 +0300

    * Начал писать опцию контроля: 135708

---
+++ app/src/main/java/ua/com/merchik/merchik/Options/Controls/OptionControlCheckingScoreDetailedReport.java
@@ -0,0 +1,18 @@
+package ua.com.merchik.merchik.Options.Controls;
+
+
+import ua.com.merchik.merchik.Options.OptionControl;
+
+/**
+ * 28.04.2023
+ * <p>
+ * ID: 135708
+ * Опция контроля
+ * Выполняется проверка УРОВНЯ ОЦЕНКИ ДетОтчета по данному посещению. Для начисления снижения
+ * исполнителю, если есть низкие оценки по его ДетОтчету
+ */
+public class OptionControlCheckingScoreDetailedReport<T> extends OptionControl {
+
+    public static int OPTION_CONTROL_PROMOTION_ID = 135708;
+
+}
