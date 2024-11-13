73bd19e27a98f6a192e5df421da7be536a5b333a
Konstantin Kurshin <kkurshin@gmail.com>
2024-11-13 16:36:01 +0100

    Fix 133381 update to 2 dates

--- app/src/main/java/ua/com/merchik/merchik/Options/Controls/OptionControlRegistrationPotentialClient.java
+++ app/src/main/java/ua/com/merchik/merchik/Options/Controls/OptionControlRegistrationPotentialClient.java
@@ -61,7 +61,7 @@ public class OptionControlRegistrationPotentialClient<T> extends OptionControl {
         }
 
         dtFrom = dt - 2592000;  // -30 дней
-        dtTo = dt + 86400;      // +1 день
+        dtTo = dt + (86400 * 2);      // +2 дня
         dateDiapason = "За период с " + Clock.getHumanTime3(dtFrom) + " по " + Clock.getHumanTime3(dtTo);
     }
 
