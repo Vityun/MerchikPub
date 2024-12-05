d6af8254b3313c8f49bef7e1d7540e848a8b6e0d
Viktor <thevityun@gmail.com>
2024-12-05 20:29:32 +0200

    До начала работ сделали как до Олександра

--- app/src/main/java/ua/com/merchik/merchik/Options/Buttons/OptionButtonPhotoBeforeStartWork.java
+++ app/src/main/java/ua/com/merchik/merchik/Options/Buttons/OptionButtonPhotoBeforeStartWork.java
@@ -1,28 +1,16 @@
 package ua.com.merchik.merchik.Options.Buttons;
 
-import static ua.com.merchik.merchik.database.room.RoomManager.SQL_DB;
-
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
-import ua.com.merchik.merchik.Activities.Features.FeaturesActivity;
+import ua.com.merchik.merchik.Activities.DetailedReportActivity.DetailedReportActivity;
 import ua.com.merchik.merchik.Globals;
+import ua.com.merchik.merchik.MakePhoto.MakePhoto;
 import ua.com.merchik.merchik.Options.OptionControl;
 import ua.com.merchik.merchik.Options.Options;
-import ua.com.merchik.merchik.R;
 import ua.com.merchik.merchik.WorkPlan;
-import ua.com.merchik.merchik.data.Database.Room.AddressSDB;
 import ua.com.merchik.merchik.data.OptionMassageType;
 import ua.com.merchik.merchik.data.RealmModels.OptionsDB;
-import ua.com.merchik.merchik.data.RealmModels.TradeMarkDB;
 import ua.com.merchik.merchik.data.RealmModels.WpDataDB;
-import ua.com.merchik.merchik.dataLayer.ContextUI;
-import ua.com.merchik.merchik.database.realm.tables.TradeMarkRealm;
-import ua.com.merchik.merchik.features.main.DBViewModels.SamplePhotoSDBViewModel;
 
 public class OptionButtonPhotoBeforeStartWork<T> extends OptionControl {
     public int OPTION_BUTTON_PHOTO_BEFORE_START_WORK_ID = 135809;
@@ -51,32 +39,32 @@ public class OptionButtonPhotoBeforeStartWork<T> extends OptionControl {
         new Globals().fixMP(wpDataDB, null);// Фиксация Местоположения в таблице ЛогМп
         try {
 
-            try {
-                AddressSDB addr = SQL_DB.addressDao().getById(wpDataDB.getAddr_id());
-                TradeMarkDB tradeMarkDB = TradeMarkRealm.getTradeMarkRowById(String.valueOf(addr.tpId));
+//            try {
+//                AddressSDB addr = SQL_DB.addressDao().getById(wpDataDB.getAddr_id());
+//                TradeMarkDB tradeMarkDB = TradeMarkRealm.getTradeMarkRowById(String.valueOf(addr.tpId));
+//
+//                Intent intent = new Intent(context, FeaturesActivity.class);
+//                Bundle bundle = new Bundle();
+//                bundle.putString("viewModel", SamplePhotoSDBViewModel.class.getCanonicalName());
+//                bundle.putString("contextUI", ContextUI.SAMPLE_PHOTO_FROM_OPTION_135809.toString());
+//                JsonObject dataJson = new JsonObject();
+//                dataJson.addProperty("tradeMarkDBId", tradeMarkDB.getID());
+//                dataJson.addProperty("wpDataDBId", String.valueOf(wpDataDB.getId()));
+//                dataJson.addProperty("optionDBId", String.valueOf(optionDB.getID()));
+//                bundle.putString("dataJson", new Gson().toJson(dataJson));
+//                bundle.putString("title", R.string.title_samplephotosdb + ", ");
+//                bundle.putString("subTitle", "В списке представлены образцы фотоотчетов. " +
+//                        "Для того, чтобы изготовить 'Фото витрины до начала работ' нажмите на соответствующую фотографию. " +
+//                        "Затем увеличьте ее до размера экрана и выполните фото, нажав на кнопку фотоаппарата в правом нижнем углу. ");
+//                intent.putExtras(bundle);
+//                context.startActivity(intent);
+//            } catch (Exception e) {
+//                Globals.writeToMLOG("ERROR", "OptionButtonPhotoBeforeStartWork/executeOption/Exception", "Exception e: " + e);
+//            }
 
-                Intent intent = new Intent(context, FeaturesActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("viewModel", SamplePhotoSDBViewModel.class.getCanonicalName());
-                bundle.putString("contextUI", ContextUI.SAMPLE_PHOTO_FROM_OPTION_135809.toString());
-                JsonObject dataJson = new JsonObject();
-                dataJson.addProperty("tradeMarkDBId", tradeMarkDB.getID());
-                dataJson.addProperty("wpDataDBId", String.valueOf(wpDataDB.getId()));
-                dataJson.addProperty("optionDBId", String.valueOf(optionDB.getID()));
-                bundle.putString("dataJson", new Gson().toJson(dataJson));
-                bundle.putString("title", R.string.title_samplephotosdb + ", ");
-                bundle.putString("subTitle", "В списке представлены образцы фотоотчетов. " +
-                        "Для того, чтобы изготовить 'Фото витрины до начала работ' нажмите на соответствующую фотографию. " +
-                        "Затем увеличьте ее до размера экрана и выполните фото, нажав на кнопку фотоаппарата в правом нижнем углу. ");
-                intent.putExtras(bundle);
-                context.startActivity(intent);
-            } catch (Exception e) {
-                Globals.writeToMLOG("ERROR", "OptionButtonPhotoBeforeStartWork/executeOption/Exception", "Exception e: " + e);
-            }
 
-
-//            MakePhoto makePhoto = new MakePhoto();
-//            makePhoto.pressedMakePhoto((DetailedReportActivity) context, wpDataDB, optionDB, "14"); // Фото До начала Работ
+            MakePhoto makePhoto = new MakePhoto();
+            makePhoto.pressedMakePhoto((DetailedReportActivity) context, wpDataDB, optionDB, "14"); // Фото До начала Работ
 
         }catch (Exception e){
             Globals.writeToMLOG("ERROR", "OptionButtonPhotoBeforeStartWork/executeOption/Exception", "Exception e: " + e);
